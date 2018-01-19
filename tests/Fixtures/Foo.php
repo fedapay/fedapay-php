c8aaa81e00d9f7105e321a7457d8122d14819fa9
shadonet <ingprs01@gmail.com>
2018-01-19 19:17:27 +0100

    Fix issue

---
+++ tests/Fixtures/Foo.php
@@ -0,0 +1,7 @@
+<?php
+namespace Tests\Fixtures;
+
+class Foo extends \Fedapay\Resource { }
+class Foo_Test extends \Fedapay\Resource { }
+class Foo_Person extends \Fedapay\Resource { }
+class Foo_Currency extends \Fedapay\Resource { }
